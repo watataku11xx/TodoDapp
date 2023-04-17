a10edb1a0916b86ca0657340b3e1f401f189e4a6
Watanabe Takuto <watataku131122@outlook.jp>
2023-04-17 17:38:59 +0900

    [add] Todo.sol, Todo.js

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -3,18 +3,28 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Todo{
-
-  int private number;
-
-  constructor(int _number){
-    number = _number;
+  struct Todo {
+    uint taskId;
+    string content;
+    bool completed;
   }
 
-  function referenceNumber() view public returns (int){
-    return number;
+  uint private number = 0;
+  Todo[] private todos;
+
+  constructor(){}
+
+  function setTodo(string memory _content) public {
+    todos.push(Todo(number, _content, false));
+    number++;
+  }
+
+  function getTodo() public view returns (Todo[] memory){
+    return todos;
+  }
+
+  function changeBool(uint _taskId) public{
+    todos[_taskId].completed = true;
   }
   
-  function setNumber(int _number) public{
-    number = _number;
-  }
 }
--- test/Todo.js
+++ test/Todo.js
@@ -4,15 +4,12 @@ const {ethers} = require('hardhat');
 describe('Todo', function() {
   it('should return correct number', async function(){
     const Todo = await ethers.getContractFactory('Todo')
-    const todo = await Todo.deploy(1);
+    const todo = await Todo.deploy();
     await todo.deployed();
 
-    expect(await todo.referenceNumber()).to.equal(1);
+    const setTodo = await todo.setTodo('English assginment');
 
-    const setNumber = await todo.setNumber(2);
-
-    await setNumber.wait();
-
-    expect(await todo.referenceNumber()).to.equal(2);
+    await setTodo.wait();
+    
   });
 });
